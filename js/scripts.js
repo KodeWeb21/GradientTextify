9758d4623d7e9da5ec910722bdcf8c5e23a0fa95
Bryant Mejia <bryantmejia21@gmail.com>
2024-03-31 22:22:02 -0400

    cambiando estado del boton al seleccionarlo

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 const $options = document.querySelector('.options');
+const btnStyleActive = "card__btn--active";
+let btnOrientationActive;
+let btnPositionActive;
+const changeStateBtn = (btnElement, prevBtn) =>{
+    if(prevBtn){
+        prevBtn.classList.remove(btnStyleActive)
+    }
+    btnElement.classList.add(btnStyleActive);
+    prevBtn = btnElement;
+    return prevBtn;
+}
+
 
 $options.addEventListener('click',(e)=>{
     const target = e.target;
    
+    if(target.matches(".btn-orientation")){
+        btnOrientationActive = changeStateBtn(target,btnOrientationActive);
+    }
+
+    if(target.matches(".btn-position")){
+        btnPositionActive = changeStateBtn(target, btnPositionActive);
+    }
 })
 
 $options.addEventListener('input',(e)=>{
