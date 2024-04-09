91c9d9c48c0f4757362fa374ae1f163d8cc35afe
Bryant Mejia <bryantmejia21@gmail.com>
2024-04-08 21:42:27 -0400

    Construyendo editor de estilo

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,36 @@ const $options = document.querySelector('.options');
 const btnStyleActive = "card__btn--active";
 let btnOrientationActive;
 let btnPositionActive;
-const changeStateBtn = (btnElement, prevBtn) =>{
-    if(prevBtn){
-        prevBtn.classList.remove(btnStyleActive)
+let position = "";
+let orientationCanva;
+const canvaText = document.querySelector('.canva__text');
+const primaryColorInput = document.querySelector('.color1');
+const secondColorInput = document.querySelector('.color2');
+const primaryRange = document.querySelector('.range1');
+const secondRange = document.querySelector('.range2');
+const applyStyleCss = () =>{
+    if(orientationCanva === 'linear'){
+        canvaText.style = ` background: #DFA3CF; background: linear-gradient(to ${position}, ${primaryColorInput.value} ${primaryRange.value}%, ${secondColorInput.value} ${secondRange.value}%);-webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        }`;
+    }else{
+        canvaText.style = `background: #DFA3CF; background: radial-gradient(to ${position}, ${primaryColorInput.value} ${primaryRange.value}%, ${secondColorInput.value} ${secondRange.value}%);-webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        }`;
     }
+}
+
+const changeStateBtn = (btnElement, prevBtn) =>{
+    if(prevBtn) prevBtn.classList.remove(btnStyleActive)
+
     btnElement.classList.add(btnStyleActive);
     prevBtn = btnElement;
+
+    if(btnElement.matches('.btn-position')) position = btnElement.dataset.position;
+
+    if(btnElement.matches('.btn-orientation')) orientationCanva = btnElement.dataset.orientation;
+
+    applyStyleCss();
     return prevBtn;
 }
 
@@ -29,10 +53,12 @@ $options.addEventListener('input',(e)=>{
     if(target.matches('.color')){
         const textColor = target.nextElementSibling;
         textColor.textContent = target.value;
+        applyStyleCss();
     }
 
     if(target.matches('.range')){
         const textRange = target.nextElementSibling;
         textRange.textContent = `${target.value}%`;
+        applyStyleCss();
     }
 })
